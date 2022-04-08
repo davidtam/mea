f449c65123a7c0d10f2e73d4251cf4a6f22ad681
David Tam <davidtam@users.noreply.github.com>
2022-04-08 08:33:20 +0100

    blacken file

--- sentiment-service/nltk_downloader.py
+++ sentiment-service/nltk_downloader.py
@@ -1,12 +1,15 @@
 import nltk
 
-nltk.download([
-    "names",
-    "stopwords",
-    "state_union",
-    "twitter_samples",
-    "movie_reviews",
-    "averaged_perceptron_tagger",
-    "vader_lexicon",
-    "punkt",
-], download_dir="/app/nltk/")
+nltk.download(
+    [
+        "names",
+        "stopwords",
+        "state_union",
+        "twitter_samples",
+        "movie_reviews",
+        "averaged_perceptron_tagger",
+        "vader_lexicon",
+        "punkt",
+    ],
+    download_dir="/app/nltk/",
+)
