04b515b331cfb76aaa802c8f477a21299d69a730
David Tam <davidtam@users.noreply.github.com>
2022-04-09 14:15:28 +0100

    rename dashboard dir and added k8s yaml

--- feedback-service/controllers/feedbackController.js
+++ feedback-service/controllers/feedbackController.js
@@ -1,4 +1,4 @@
-import Feedback from "../models/Feedback.js";
+import Feedback from "../models/feedback.js";
 import fetch from "node-fetch";
 import { SENTIMENT_URI } from "../config/environment.js";
 
